6366984668e8066ce4c4e64ac6548766166d59e3
William Burns <burnswilliam77@gmail.com>
2022-06-17 15:14:33 -0400

    first commit

---
+++ src/components/Home/Home.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import Particle from "../Particle";
+import Home2 from "./Home2";
+import Type from "./Type";
+
+function Home() {
+  return (
+    <section>
+      <Container fluid className="home-section" id="home">
+        <Particle />
+        <Container className="home-content">
+          <Row>
+            <Col md={7} className="home-header">
+              <h1 style={{ paddingBottom: 10 }} className="heading">
+                Hi All, I'm{" "}
+              </h1>
+
+              <h1 className="heading-name">
+                <strong className="main-name">William Burns</strong>
+              </h1>
+
+              <div style={{ padding: 45, textAlign: "left" }}>
+                <Type />
+              </div>
+            </Col>
+          </Row>
+          <Row>
+          <div style={{display: 'flex', justifyContent: 'center', padding: 175}}>
+            <div class="scroll-downs" >
+              <div class="mousey">
+                <div class="scroller"></div>
+              </div>
+            </div>
+          </div>
+          </Row>
+        </Container>
+      </Container>
+      <Home2 />
+    </section>
+  );
+}
+
+export default Home;
---
+++ src/components/Particle.js
@@ -0,0 +1,79 @@
+import React from "react";
+import Particles from "react-tsparticles";
+
+function Particle() {
+  return (
+    <Particles
+      id="tsparticles"
+      options={{
+        fpsLimit: 120,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: "push",
+            },
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true,
+          },
+          modes: {
+            push: {
+              quantity: 4,
+            },
+            repulse: {
+              distance: 100,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#243a47",
+          },
+          links: {
+            color: "#243a47",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 0.5,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outModes: {
+              default: "bounce",
+            },
+            random: false,
+            speed: 0.2,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            value: { min: 0.1, max: 1 },
+          },
+        },
+        detectRetina: true,
+      }}
+    />
+  );
+}
+
+export default Particle;
